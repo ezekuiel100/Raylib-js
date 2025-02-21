62f6bc8db93338621dfee21e30d5ab03737b8c76
unknown <ezekuiel100@gmail.com>
2025-02-21 13:44:56 -0300

    isMouseButtonDown method

--- script.js
+++ script.js
@@ -6,6 +6,18 @@ class RayLibJS {
     this.canvas.width = width;
     this.canvas.height = height;
 
+    this.mouseLeftButton = 0;
+    this.mouseRightButton = 2;
+    this.mouseDown = null;
+
+    this.canvas.addEventListener("mousedown", (e) => {
+      this.mouseDown = e.button;
+    });
+
+    this.canvas.addEventListener("mouseup", () => {
+      this.mouseDown = null;
+    });
+
     this.canvas.style.border = "1px solid black";
     this.ctx = this.canvas.getContext("2d");
   }
@@ -36,16 +48,35 @@ class RayLibJS {
     this.ctx.fillText(text, x, y);
   }
 
-  clearBackgound(color) {
+  clearBackground(color) {
     this.ctx.clearRect(0, 10, this.canvas.width, this.canvas.height);
     this.ctx.fillStyle = color;
     this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
   }
+
+  isKeyPressed() {}
+
+  isMouseButtonDown(mouseButton) {
+    return this.mouseDown === mouseButton;
+  }
 }
 
-const ray = new RayLibJS(800, 700);
-ray.drawRectangle(10, 10, 100, 300, "red");
-ray.drawCircle(200, 80, 70, "blue"); // centerX, centerY, radius, color
-ray.drawLine(100, 100, 800, 450, "green"); //startX, startY, endX, endY, color
-ray.drawText("ola mundo", 0, 90, 50, "black"); //text, x, y, fontSize, color
-ray.clearBackgound("blue");
+const rl = new RayLibJS(800, 700);
+
+function loop() {
+  rl.clearBackground("white");
+
+  rl.drawRectangle(10, 10, 100, 300, "red");
+  rl.drawCircle(200, 80, 70, "blue"); // centerX, centerY, radius, color
+  rl.drawLine(100, 100, 800, 450, "green"); //startX, startY, endX, endY, color
+  rl.drawText("ola mundo", 0, 90, 50, "black"); //text, x, y, fontSize, color
+
+  if (rl.isMouseButtonDown(rl.mouseLeftButton)) {
+    console.log("Botão Esquerdo Clicado! 🖱");
+  }
+  if (rl.isMouseButtonDown(rl.mouseRightButton)) {
+    console.log("Botão Direito Clicado! 🖱");
+  }
+  requestAnimationFrame(loop);
+}
+loop();
